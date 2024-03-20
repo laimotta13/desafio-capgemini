80706d05db8ff905ecac9ce5842b15e378d0eff8
Laís Motta <97487488+laimotta13@users.noreply.github.com>
2024-03-20 00:32:17 -0300

    Update mostra_repetidos.py
    Depois de estudar mais, consegui corrigir o código.

--- mostra_repetidos.py
+++ mostra_repetidos.py
@@ -1,21 +1,9 @@
-lista1 = input("Digite uma palavra: ")
+palavra = input("Digite uma palavra: ")
 
-lista1 = list(lista1)
+quantidade_diagramas = 0
 
-lista2 = []
+for i in range(len(palavra) - 1):
+    if palavra[i] != ' ' and palavra[i+1] != ' ':  # Ignora espaços em branco
+        quantidade_diagramas += 1
 
-i = 0
-
-y = 0
-
-for i in range(0, len(lista1)):
-    if y == lista1[i]:
-       lista2.append(lista1[i])
-       i = i + 1
-    else:
-        y = y + 1
-        
-        
-        
-print(len(lista2))
-
+print("Quantidade de diagramas:", quantidade_diagramas)
